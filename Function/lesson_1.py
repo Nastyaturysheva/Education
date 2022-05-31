9278f5fef4c23fb6e338632d8d4e8872dd46609c
Турышева Анастасия Александровна <Turysheva.An.A@sberbank.ru>
2022-05-31 11:01:38 +0300

    Разобрали синтаксис создания функций, разобрали тайп хинтинги, написали функцию факториал.
    Разобрали задачу про Високосный год, запустили четыре теста на определение является ли год високосным или нет

--- Function/lesson_1.py
+++ Function/lesson_1.py
@@ -37,3 +37,39 @@ def factorial(r: int) -> int:
 print('3! =', factorial(3))
 print('0! =', factorial(0))
 print('9! =', factorial(9))
+
+
+def is_leap_year(y: int) -> None:
+    if (y % 4 == 0) and (y % 100 != 0):
+        print('високосный год')
+        print('делится на четыре и не делится на 100')
+    elif y % 400 == 0:
+        print('високосный год')
+        print('делится на 400')
+    else:
+        print('не високосный год')
+        if y % 4 != 0:
+            print('не делится 4')
+        elif y % 100 == 0:
+            print('делится на 100 и не делится на 400')
+
+
+is_leap_year(2020)
+is_leap_year(2100)
+is_leap_year(2800)
+is_leap_year(2677)
+
+def annuity_credit(body: float, month_number: int, rate: float) -> float:
+    """
+    Расчет потребительского кредита с % ставкой, который вернет ежемесячный аннуитетнтный платеж
+    Parameters
+    ----------
+    body
+    month_number
+    rate
+
+    Returns
+    -------
+
+    """
+
